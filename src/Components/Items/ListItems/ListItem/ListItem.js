87eaae72e4e725d750bff032f57e6b99b3267cad
Kruteleff Teemu <teemu.kruteleff@empower.fi>
2020-07-10 14:17:25 +0300

    Refactore listitems and listitem components

--- src/Components/Items/ListItems/ListItem/ListItem.js
+++ src/Components/Items/ListItems/ListItem/ListItem.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Animated} from 'react-native';
+
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 const ListItem = props => {
+  console.log(props);
+
+  const LeftActions = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: 'clamb',
+    });
+
+    return (
+      <View style={styles.leftAction}>
+        <Animated.Text style={[styles.actionText, {transform: [{scale}]}]}>
+          Delete item
+        </Animated.Text>
+      </View>
+    );
+  };
+
   return (
-    <>
-      <Text style={styles.itemText}>{props.children}</Text>
-    </>
+    <Swipeable
+      renderLeftActions={LeftActions}
+      onSwipeableLeftOpen={() => alert('Delete Item')}>
+      <TouchableOpacity style={styles.FlatListView}>
+        <Text style={styles.itemText}>{props.children}</Text>
+      </TouchableOpacity>
+    </Swipeable>
   );
 };
 
@@ -14,6 +39,19 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  FlatListView: {
+    padding: 5,
+  },
+  leftAction: {
+    backgroundColor: '#dd2c00',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  actionText: {
+    color: '#fff',
+    fontWeight: '600',
+    padding: 20,
   },
 });
 
--- src/Components/Items/ListItems/ListItems.js
+++ src/Components/Items/ListItems/ListItems.js
@@ -1,30 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import {
-  View,
-  FlatList,
-  ActivityIndicator,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import {View, FlatList, ActivityIndicator, StyleSheet} from 'react-native';
 
 import firestore from '@react-native-firebase/firestore';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 import ListItem from './ListItem/ListItem';
 
 const ListItems = props => {
   const [loading, setLoading] = useState(true);
   const [items, setItems] = useState([]);
-  const [checked, setChecked] = useState(true);
-
-  const renderLeftActions = (progress, dragX) => {
-    return (
-      <View style={styles.leftAction}>
-        <Text style={styles.actionText}>Delete item</Text>
-      </View>
-    );
-  };
 
   useEffect(() => {
     const subscriber = firestore()
@@ -67,7 +50,8 @@ const ListItems = props => {
 
   const deleteItemHandler = item => {
     // Delete item function here
-    firestore()
+    console.log('Log from deleteItemHandler', item);
+    /*firestore()
       .collection('lists')
       .doc(props.docId)
       .collection('shoppingItems')
@@ -75,7 +59,7 @@ const ListItems = props => {
       .delete()
       .then(() => {
         console.log('Item deleted ', item.key);
-      });
+      });*/
   };
 
   const updateItemHandler = item => {
@@ -89,7 +73,13 @@ const ListItems = props => {
         style={styles.FlatList}
         data={items}
         renderItem={({item, index}) => (
-          <Swipeable renderLeftActions={renderLeftActions}>
+          <ListItem>
+            {item.name}
+            {item.amount}
+          </ListItem>
+          /*<Swipeable
+            renderLeftActions={renderLeftActions}
+            onSwipeableLeftOpen={() => deleteItemHandler(item)}>
             <TouchableOpacity
               style={styles.FlatListView}
               {...item}
@@ -99,7 +89,7 @@ const ListItems = props => {
               <ListItem>{item.name.toUpperCase()}</ListItem>
               <ListItem>{item.amount}</ListItem>
             </TouchableOpacity>
-          </Swipeable>
+          </Swipeable>*/
         )}
         keyExtractor={item => item.key}
       />
@@ -112,22 +102,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     marginTop: 5,
-  },
-  FlatListView: {
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 5,
-    padding: 5,
-  },
-  leftAction: {
-    backgroundColor: '#388e3c',
-    justifyContent: 'center',
-    flex: 1,
-  },
-  actionText: {
-    color: '#fff',
-    fontWeight: '600',
-    padding: 20,
   },
 });
 
